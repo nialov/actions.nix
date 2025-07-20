20f152ebc17fad881ca3be20e1653b986754e0e1
nialov <nikolasovaskainen@gmail.com>
2025-07-20 12:32:55 +0300

    style: format with nixfmt-rfc-style

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,24 +1,28 @@
 {
-  perSystem = { config, pkgs, ... }: {
-    devShells = {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [ pre-commit ];
-        shellHook = config.pre-commit.installationScript;
-      };
-    };
-    pre-commit = {
-      check.enable = true;
-      settings = {
-        src = ../.;
-        hooks = {
-          nixfmt-rfc-style.enable = true;
-          statix.enable = true;
-          deadnix.enable = true;
-          commitizen.enable = true;
-          trim-trailing-whitespace = { enable = true; };
+  perSystem =
+    { config, pkgs, ... }:
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ pre-commit ];
+          shellHook = config.pre-commit.installationScript;
         };
       };
+      pre-commit = {
+        check.enable = true;
+        settings = {
+          src = ../.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+            commitizen.enable = true;
+            trim-trailing-whitespace = {
+              enable = true;
+            };
+          };
+        };
 
+      };
     };
-  };
 }
