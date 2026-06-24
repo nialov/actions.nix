fdb2ebc1c12c4b097a1dc850ce4955e7dcf5df4b
nialov <nikolasovaskainen@gmail.com>
2026-06-24 15:34:03 +0300

    refactor: try to separate flake-parts logic from generic module logic

--- nix/flake-modules/actions-nix/ci.nix
+++ nix/flake-modules/actions-nix/ci.nix
@@ -81,33 +81,6 @@ let
 
   ciModule = {
     options = {
-      pre-commit = {
-        enable = lib.mkEnableOption ''
-          pre-commit generation of workflow yaml files.
-
-          The pre-commit hook, generated using
-          [git-hooks.nix](https://github.com/cachix/git-hooks.nix), converts
-          `flake.actions-nix.workflows` configuration into respective workflow
-          files in the path defined within the configuration, i.e. key in
-          `actions-nix.workflows` attribute set
-        '';
-      };
-      useJJ = lib.mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to use jj (Jujutsu) for repository root detection
-          instead of git during workflow rendering. When enabled, jj will be
-          used to find the repository root and fallback to git as normal. This
-          is useful if you use jj workspaces where there isn't a git root
-          available.
-        '';
-      };
-      # render-package = {
-      #   enable = lib.mkEnableOption ''
-      #     addition of a package definition to `perSystem.packages.render-workflows` for rendering workflows.
-      #   '';
-      # };
       workflows = lib.mkOption {
         type = types.attrsOf (lib.types.submoduleWith { modules = [ workflowsModule ]; });
         default = { };
